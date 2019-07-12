2baf47699525ab7635150c8e80d194f93640014b
Yushi Yang <yangyushi1992@icloud.com>
2019-07-12 16:42:44 +0100

    v21

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'readme.md'), encoding='utf-8') as f:
 
 setup(
     name='read_lif',
-    version='0.2.0',
+    version='0.2.1',
     description='A Python module for loading lif file as numpy array',
     long_description=long_description,
     long_description_content_type='text/markdown',
